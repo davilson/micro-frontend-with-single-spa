98c745e3abe07daeba71900731ccb6607a092660
Davilson de Castro <davilsondecastro@gmail.com>
2021-10-11 20:01:12 -0300

    feat: adiciona micro front-end financeiro

---
+++ packages/supermarket-financial/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Switch } from "react-router-dom";
+
+import Route from "./Route";
+import DefaultTemplate from "../templates/Default";
+
+export default function Routes() {
+  return (
+    <BrowserRouter basename="financial">
+      <Switch>
+        <Route path="/" template={DefaultTemplate} exact={true}>
+          <h1>Financeiro</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
