a5bd26b84318d918c1f4da830b03d8416868956e
Davilson de Castro <davilsondecastro@gmail.com>
2021-10-11 21:09:12 -0300

    refactor: ajusta rotas para carregar os parsels

--- packages/supermarket-financial/src/routes/index.tsx
+++ packages/supermarket-financial/src/routes/index.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Switch } from "react-router-dom";
+import { foxkeyComponents } from "@foxkey/design-system";
 
 import Route from "./Route";
 import DefaultTemplate from "../templates/Default";
 
 export default function Routes() {
+  const { Title } = foxkeyComponents;
   return (
     <BrowserRouter basename="financial">
       <Switch>
         <Route path="/" template={DefaultTemplate} exact={true}>
-          <h1>Financeiro</h1>
+          <Title>Financeiro</Title>
         </Route>
       </Switch>
     </BrowserRouter>
